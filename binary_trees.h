54d5669ffd0e07955a4a0cb797ae9287f3a4d246
ADEODATUS NKUNDIMANA <a.nkundiman@alustudent.com>
2026-05-17 15:51:57 +0200

    Add sibling and uncle functions to binary trees header

--- binary_trees.h
+++ binary_trees.h
@@ -80,4 +80,10 @@ int binary_tree_is_full(const binary_tree_t *tree);
 /* Task 16 */
 int binary_tree_is_perfect(const binary_tree_t *tree);
 
+/* Task 17 */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+
+/* Task 18 */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
 #endif /* BINARY_TREES_H */
